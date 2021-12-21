3d28ff718b23c6c20128f3c069f110e8a15b0ca3
A-F-K-hub <62518244+A-F-K-hub@users.noreply.github.com>
2021-12-21 11:35:40 +0200

    chat functionality complete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
@@ -13,7 +15,13 @@ export default function Home() {
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={logout}>Logout</button>
+
+      <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
+        <div className="max-w-screen-2xl mx-auto">
+          <Header />
+          <Messages />
+        </div>
+      </div>
     </div>
   );
 }
